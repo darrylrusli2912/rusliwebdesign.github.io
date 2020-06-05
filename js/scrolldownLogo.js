8afec2c4f204e32fbfaac536d45dd54f8d37a6e4
darrylrusli2912 <65494437+darrylrusli2912@users.noreply.github.com>
2020-06-05 17:31:20 +1000

    update scrolling js

--- js/scrolldownLogo.js
+++ js/scrolldownLogo.js
@@ -8,13 +8,11 @@ function scrollLogoFunction() {
 	if(document.body.scrollTop>40 || document.documentElement.scrollTop> 10){
 		document.getElementById("logo").style.width="40px";
 		document.getElementById("menuDesktopNav").style.paddingTop="10px";
-		document.getElementById("menu-toggle").style.fontSize="1.75em";
 		document.getElementById("menu-bar").style.paddingTop="0";
 	}
 	else{
 		document.getElementById("logo").style.width="80px";
 		document.getElementById("menuDesktopNav").style.paddingTop ="25px";
-		document.getElementById("menu-toggle").style.fontSize="2.5em";
 		document.getElementById("menu-bar").style.paddingTop="15px";	
 	}
 	}
